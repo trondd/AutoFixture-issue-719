677d767fceb1f4f04975990bc86db28ffda00d8d
Trond Danielsen <tda@dips.no>
2016-11-10 11:42:24 +0100

    Failing test

--- AutoFixture.Bug/UnitTest1.cs
+++ AutoFixture.Bug/UnitTest1.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq.Expressions;
+using Ploeh.AutoFixture;
+using Xunit;
 
 namespace AutoFixture.Bug
 {
-    [TestClass]
+    public interface IRule
+    {
+        bool Evaluate(string arg);
+    }
+
+    public class Rule : IRule
+    {
+        public Expression<Func<string, bool>> Condition;
+
+        public Rule(Expression<Func<string, bool>> condition)
+        {
+            Condition = condition;
+        }
+
+        public bool Evaluate(string arg)
+        {
+            return Condition.Compile().Invoke(arg);
+        }
+    }
+
+    public class MyRule : Rule
+    {
+        public MyRule() : base(s => InternalCondition(s))
+        {
+        }
+
+        private static bool InternalCondition(string s)
+        {
+            throw new Exception();
+        }
+    }
+
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
+            var fixture = new Fixture();
+
+            var sut = fixture.Create<MyRule>();
+
+            // Inspection of the sut.Condition field reveals that the has changed to {Param_0 => true}
+            // Question is why AutoFixture is altering this value?
+
+            Assert.Throws<Exception>(() => sut.Evaluate(string.Empty));
         }
     }
 }
